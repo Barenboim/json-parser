5ce852b7988df1a5d75eaba9f79ff20b6ecb40e3
XieHan <XieHan@sogou-inc.com>
2022-02-16 00:37:06 +0800

    update test to print json document for a json value

--- test.c
+++ test.c
@@ -1,8 +1,123 @@
-#include "json_parser.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "json_parser.h"
 
 #define BUFSIZE		(8 * 1024 * 1024)
+
+void print_json_value(const json_value_t *val, int depth);
+
+void print_json_object(const json_object_t *obj, int depth)
+{
+	const char *name;
+	const json_value_t *val;
+	int n = 0;
+	int i;
+
+	printf("{\n");
+	json_object_for_each(name, val, obj)
+	{
+		if (n != 0)
+			printf(",\n");
+		n++;
+		for (i = 0; i < depth + 1; i++)
+			printf("    ");
+		printf("\"%s\": ", name);
+		print_json_value(val, depth + 1);
+	}
+
+	printf("\n");
+	for (i = 0; i < depth; i++)
+		printf("    ");
+	printf("}");
+}
+
+void print_json_array(const json_array_t *arr, int depth)
+{
+	const json_value_t *val;
+	int n = 0;
+	int i;
+
+	printf("[\n");
+	json_array_for_each(val, arr)
+	{
+		if (n != 0)
+			printf(",\n");
+		n++;
+		for (i = 0; i < depth + 1; i++)
+			printf("    ");
+		print_json_value(val, depth + 1);
+	}
+
+	printf("\n");
+	for (i = 0; i < depth; i++)
+		printf("    ");
+	printf("]");
+}
+
+void print_json_string(const char *str)
+{
+	printf("\"");
+	while (*str)
+	{
+		switch (*str)
+		{
+		case '\r':
+			printf("\\r");
+			break;
+		case '\n':
+			printf("\\n");
+			break;
+		case '\f':
+			printf("\\f");
+			break;
+		case '\b':
+			printf("\\b");
+			break;
+		case '\"':
+			printf("\\\"");
+			break;
+		case '\t':
+			printf("\\t");
+			break;
+		case '\\':
+			printf("\\\\");
+			break;
+		default:
+			printf("%c", *str);
+			break;
+		}
+		str++;
+	}
+	printf("\"");
+}
+
+void print_json_value(const json_value_t *val, int depth)
+{
+	switch (json_value_type(val))
+	{
+	case JSON_VALUE_STRING:
+		print_json_string(json_value_string(val));
+		break;
+	case JSON_VALUE_NUMBER:
+		printf("%lf", json_value_number(val));
+		break;
+	case JSON_VALUE_OBJECT:
+		print_json_object(json_value_object(val), depth);
+		break;
+	case JSON_VALUE_ARRAY:
+		print_json_array(json_value_array(val), depth);
+		break;
+	case JSON_VALUE_TRUE:
+		printf("true");
+		break;
+	case JSON_VALUE_FALSE:
+		printf("false");
+		break;
+	case JSON_VALUE_NULL:
+		printf("null");
+		break;
+	}
+}
 
 int main()
 {
@@ -20,13 +135,12 @@ int main()
 	json_value_t *val = json_value_create(buf);
 	if (val)
 	{
-		printf("success!\n");
+		print_json_value(val, 0);
 		json_value_destroy(val);
 	}
 	else
-	{
-		printf("failed!\n");
-	}
+		fprintf(stderr, "Invalid JSON document.\n");
+
 	return 0;
 }
 
